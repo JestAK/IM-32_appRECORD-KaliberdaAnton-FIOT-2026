bb84759f6ab958dcce806642785c9b3f91504e95
Kaliberda Anton <73352390+JestAK@users.noreply.github.com>
2026-05-14 04:31:38 +0200

    fix: fix path

--- base-path.js
+++ base-path.js
@@ -1,3 +1,3 @@
 export const BASE_PATH =
   process.env.BASE_PATH ??
-  (process.env.NODE_ENV === "production" ? "IM-32_appRECORD-KaliberdaAnton-FIOT-2026" : "");
+  (process.env.NODE_ENV === "production" ? "/IM-32_appRECORD-KaliberdaAnton-FIOT-2026" : "");
